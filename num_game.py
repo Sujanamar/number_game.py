20eae00a79ebf8302880fec1f2340a03c4f05a63
Sujanamar <amarsujana04@gmail.com>
2025-07-26 22:06:23 +0530

    number edited

--- num_game.py
+++ num_game.py
@@ -1,9 +1,11 @@
-import random
 
-  secret_number = random.randomint(1,10)
+
+import random;
+
+  secret_number = random.randomint(1,100)
   
 
-  print("guess the number (between 1 and 10):")
+  print("guess the number (between 1 and 100):")
 
   while True:
       guess=int(input("your guess:"))
